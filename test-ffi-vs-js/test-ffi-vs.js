5c9427f2a174da609794549b548471457a327fd0
Dmitry Sigaev <Dmitry_Sigaev@epam.com>
2016-11-23 00:03:18 +0300

    change api in output json object score_table.txt

--- test-ffi-vs-js/test-ffi-vs.js
+++ test-ffi-vs-js/test-ffi-vs.js
@@ -48,16 +48,9 @@ var substitution = test.read_substitution_matrix(gaptest_utest);
 var profile = test.set_profile(-1, 0, substitution);
 //var seq1 = "CTTCCTACGTTGGGTCATAGTAGTGCGGCGTGGGCAATGCCTACGGAGGGGTGGAGCAACTGGCGCTATCACTTCTACCATCGTCTGCAGCGTACGA";
 //var seq2 = "tcgtacgctgcagacgatggtagaagtgatagcgccagttgctccacccctccgtaggcattgcccacgccgcactactatgacccaacgtaggaagttg";
-var seq1 = "CAACTTCCTGGCGCTATCACTTCTACCATCGTCTGCAGCGT";
-var seq2 = "acgatggtagaagtgatagcgccagttgctccacccct";
+var seq2 = "CAACTTCCTGGCGCTATCACTTCTACCATCGTCTGCAGCGT";
+var seq1 = "acgatggtagaagtgatagcgccagttgctccacccct";
 var res = test.sw_directions(profile, seq1, seq2);
-var full_object = { score: res[0], trace: res[1], seq1: seq1, seq2: seq2 };
-var score_table = JSON.stringify(full_object);
-fs.writeFile('score_table.txt', score_table, function (err) {
-	if (err)
-		return console.log(err);
-	console.log('Hello World > helloworld.txt');
-});
 
 //var score = Math.max.apply(null, res[0]);
 var score = res[0][0][0];
@@ -67,6 +60,15 @@ for (var i = 0; i < res[0].length; i++) {
 		score = Math.max.apply(Math, elementArray);
 	}
 }
+
+var full_object = { score: score, scorematrix: res[0], directions: res[1], seq1: seq2, seq2: seq1 };
+var score_table = JSON.stringify(full_object);
+fs.writeFile('score_table.txt', score_table, function (err) {
+    if (err)
+        return console.log(err);
+    console.log('Hello World > helloworld.txt');
+});
+
 
 test.EXPECT("';' == test.charFunc('\u263B', out)", out2);
 test.EXPECT("';' == out.pop()", out2);
